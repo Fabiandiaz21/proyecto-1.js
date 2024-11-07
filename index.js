937789535cdac32eaa4ed0a754f43d6fcefb1bc9
Fabiandiaz21 <fsantiagodiazc21@gmail.com>
2024-11-07 07:08:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import movimientos from './routers/movimiento.js'
+import articulos from "./routers/articulos.js"
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express()
+app.use(express.json())
+app.use("/api/movimientos",movimientos)
+app.use("/api/articulos",articulos)
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
+    mongoose.connect(process.env.CNX_MONGO)
+    .then(() => console.log('Connected!'))
+    .catch((error)=> console.log(error))
+})
