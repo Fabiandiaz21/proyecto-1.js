f067ff9f719eac0c152bd074dec5224f7604afab
ortiz-moncada <ortizmoncada2006@gmail.com>
2024-11-14 08:32:29 -0500

    chech de categorias

--- routers/categorias.js
+++ routers/categorias.js
@@ -1,25 +1,41 @@
 import {Router} from "express";
+import {helpercategoria} from "../helpers/categorias.js"
 import httpcategorias from "../controllers/categorias.js"
 
 const router = Router();
 
 //crear una nueva categoria
-router.post("/",httpcategorias.postcategorias);
+router.post("/",[
+    check("descripcion","El campo descripcion es obligatorio ").notEmpty(),
+    check("estado","el estado es obligatorio").notEmpty()
+],httpcategorias.postcategorias);
 
 //modificar una categoria
-router.put("/modificar/:id",httpcategorias.putcategorias);
+router.put("/modificar/:id",[
+    check("id","el id no es valido").isMongoId(),
+    check("id","El id no existe").custom(helpercategoria.validarId)
+],httpcategorias.putcategorias);
 
 //listar todos 
 router.get("/",httpcategorias.getcategorias);
 
 //listar una categoria por ID
-router.get("/:id",httpcategorias.getcategoriasById);
+router.get("/:id",[
+    check("id","el id no es valido").isMongoId(),
+    check("id","El id no existe").custom(helpercategoria.validarId)
+],httpcategorias.getcategoriasById);
 
 //Activar
-router.put("/:id/activar",httpcategorias.putActivarCategorias);
+router.put("/:id/activar",[
+    check("id","el id no es valido").isMongoId(),
+    check("id","El id no existe").custom(helpercategoria.validarId)
+],httpcategorias.putActivarCategorias);
 
 //Desactivar
-router.put("/:id/anulados",httpcategorias.putInactivarCategorias);
+router.put("/:id/anulados",[
+    check("id","el id no es valido").isMongoId(),
+    check("id","El id no existe").custom(helpercategoria.validarId)
+],httpcategorias.putInactivarCategorias);
 
 //listar activos
 router.get("/aprobados/AP", httpcategorias.getActivos);
